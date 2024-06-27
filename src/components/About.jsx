ff388abbb2a121b3f06ab361af7725651bb82a0d
S.Y.T.D.Bandara <99232129+IT21185052@users.noreply.github.com>
2024-06-27 13:47:50 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
           <div className="p-2">
             <motion.div className="text-gray-300 my-3" >
               <motion.p className="text-justify leading-7 w-11/12 mx-auto" initial={{x: -100, opacity:0}} whileInView={{x:0,opacity:1}} transition={{delay:0.2,x:{type:"spring",stiffness:60},opacity:{duration:1},ease:"easeIn",duration:1,}}>
-              Welcome to my portfolio website! I'm a passionate BSc (Hons) Software Engineering student at the Sri Lanka Institute of Information Technology, equipped with a robust skill set in Java, JavaScript, and React. I thrive on leveraging frameworks like the MERN stack to craft seamless software solutions. With expertise in CSS frameworks, including Bootstrap5, and proficiency in SQL databases, I'm adept at delivering integrated software products that captivate users.
+              Welcome to my portfolio website! I'm a passionate BSc (Hons) Software Engineering student at the Sri Lanka Institute of Information Technology, equipped with a robust skill set in Java, JavaScript, Html,CSS, Node and React. With expertise in CSS frameworks, including Bootstrap5, and proficiency in SQL databases, I'm adept at delivering integrated software products that captivate users.
 
 I'm excited about the opportunity to translate my academic knowledge into real-world impact and contribute effectively to your team's success.
              </motion.p>
